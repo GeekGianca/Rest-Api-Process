401e38dd32f715d85d0e0596647647685e2b9ad7
GeekGianca <giancarlos.cuello@gmail.com>
2018-10-09 23:20:39 -0500

    Fix models and Repositories

--- src/main/java/com/gksoftware/processrestapi/model/ProcessList.java
+++ src/main/java/com/gksoftware/processrestapi/model/ProcessList.java
@@ -4,6 +4,8 @@ import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper;
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement;
 import com.gksoftware.processrestapi.entity.ProcessEntity;
 
@@ -14,6 +16,8 @@ public class ProcessList implements Serializable {
 	 * @serialVersion
 	 */
 	private static final long serialVersionUID = 1L;
+	@JacksonXmlProperty(localName = "ProcessList")
+    @JacksonXmlElementWrapper(useWrapping = false)
 	private List<ProcessEntity> processList = new ArrayList<>();
 	
 	public List<ProcessEntity> getProcessList() {
